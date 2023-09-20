290daac7f04523ce1641c28e2f29e140583fc4a9
SolidEiEn <aol0015@gmail.com>
2023-09-20 12:50:06 -0500

    update CSS, create JS

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+//Funcion up-arrow
+
+const up = document.querySelector("#up");
+up.addEventListener("click", ()=>{
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+});
+
+//Activar la barra hamburguesa
+const burguer = document.querySelector("#menu-hamburguesa");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burguer.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+//Cerrar menú cuando se hace click en un enlace
+//Seleccionar enlace de navegación
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+})
